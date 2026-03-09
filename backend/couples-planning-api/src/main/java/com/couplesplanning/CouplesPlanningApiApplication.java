075fb531ce2c3e18fd488f3aa1a75d94acb5f67d
Jonas Albuquerque <jonas-junior@uol.com.br>
2026-03-09 12:52:25 -0300

    chore: bootstrap spring boot backend with postgres and flyway

---
+++ backend/couples-planning-api/src/main/java/com/couplesplanning/CouplesPlanningApiApplication.java
@@ -0,0 +1,13 @@
+package com.couplesplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouplesPlanningApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouplesPlanningApiApplication.class, args);
+	}
+
+}
---
+++ backend/couples-planning-api/src/test/java/com/couplesplanning/CouplesPlanningApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couplesplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouplesPlanningApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
